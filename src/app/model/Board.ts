480b28621944042bacd61ed1d1db41c8df1fa6ed
Xynt <xabirlo@gmail.com>
2020-12-12 23:33:53 +0100

    Add board

---
+++ src/app/model/Board.ts
@@ -0,0 +1,9 @@
+import ChessPiece from './ChessPiece';
+
+export default class Board {
+  private pieceList: ChessPiece[];
+
+  constructor() {
+    this.pieceList = [];
+  }
+}
---
+++ src/app/view/board/Board.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+export default function Board() {
+  return (
+    <div>
+      <h1>Board:</h1>
+      <div className="board">
+        <div className="chessboard">
+          <div className="chessboardRow">
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+          </div>
+
+          <div className="chessboardRow">
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+          </div>
+
+          <div className="chessboardRow">
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+          </div>
+
+          <div className="chessboardRow">
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+          </div>
+
+          <div className="chessboardRow">
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+          </div>
+
+          <div className="chessboardRow">
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+          </div>
+
+          <div className="chessboardRow">
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+          </div>
+
+          <div className="chessboardRow">
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+            <div className="black-box" />
+            <div className="white-box" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
