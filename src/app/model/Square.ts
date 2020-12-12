e5b4595b0c04b15c0308b26a38d9cf3d8ebaf2f7
MarlonLoretz <moltgott@gmail.com>
2020-12-13 00:47:47 +0100

    SquareInterface and ChessPiece

---
+++ src/app/model/Square.ts
@@ -0,0 +1,10 @@
+export default class Square {
+  readonly rowI: number;
+
+  readonly columnI: number;
+
+  constructor(rowI: number, columnI: number) {
+    this.rowI = rowI;
+    this.columnI = columnI;
+  }
+}
